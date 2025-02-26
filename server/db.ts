49a329b22e16e8b208268e44e98ce915c447435a
auvoto <auvoto@gmail.com>
2025-02-27 00:17:19 +0530

    add database and nickname also optimized queries

--- server/db.ts
+++ server/db.ts
@@ -3,6 +3,8 @@ import { drizzle } from 'drizzle-orm/neon-serverless';
 import ws from "ws";
 import * as schema from "@shared/schema";
 import NodeCache from "node-cache";  // Changed to default import
+import dotenv from 'dotenv';
+dotenv.config();
 
 neonConfig.webSocketConstructor = ws;
 
